37901262f9760e0660f5b50c8a6ef46a21ad0e22
IvanEze33 <ivantortorello33@gmail.com>
2024-10-12 15:34:15 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
             return false;
         }
 
-        const phone = '5491160106858';      
+        const phone = '5491153314034';      
 let msg = ' 👑 ¡Hola! Quiero mi usuario y el bonus de bienvenida:\n';
         msg += '*Nombre:* ' + user.value + "\n";
        
